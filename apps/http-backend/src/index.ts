2126c7667fb282d3ecebccac9d7617a31939afb3
Aryan <bhardwaj7a@gmail.com>
2025-06-09 10:53:24 +0530

    Write the crude logic for http backend as well as websocket backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,3 +1,33 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { middleware } from "./middleware";
 const app = express();
+app.use(express.json());
+
+app.post("/signup" , (req,res) => {
+    //db call
+    res.json({
+        userId : 123
+    })
+
+})
+app.post("/signin" , (req,res) => {
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    },JWT_SECRET);
+
+    res.json({
+        token
+    })
+
+})
+app.post("/room", middleware , (req,res) => {
+//do a database call here
+
+res.json({
+    roomId : 123
+})
+})
 app.listen(3001);
